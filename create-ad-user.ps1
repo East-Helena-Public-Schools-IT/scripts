f5047245867cca33b57f7df2286c5a163b642773
Oliver Atkinson <oatkinson@ehps.k12.mt.us>
2024-01-10 09:46:02 -0700

    change logic to allow for more account types, add non-classified staff support

--- create-ad-user.ps1
+++ create-ad-user.ps1
@@ -5,7 +5,6 @@ $LNAME=$null
 $GRADY=$null
 # Calculate these later
 $EMAIL=$null
-$IS_TEACHER=$null
 $HOME_PATH=$null
 $GROUPS = @()
 $OU=$null
@@ -13,16 +12,11 @@ $CHANGE_PASSWORD_AT_LOGON=$null
 $DESCRIPTION=""
 
 function Set-GradYear() {
-    Write-Host "Enter Graduation Year. If this is a teacher, just press enter."
+    Write-Host "Enter Graduation Year."
     $script:GRADY = Read-Host "YYYY"
 
-    if ($GRADY.Length -eq 0) {
-        Write-Host "Creating Teacher..."
-        $script:IS_TEACHER = $true
-        return
-    } else { $script:IS_TEACHER = $false }
     if ($GRADY.Length -ne 4) {
-        Write-Host "Please use YYYY format."
+        Write-Host "Invalid format '$GRADY' Please use YYYY format."
         Set-GradYear
     }
 }
@@ -36,55 +30,83 @@ function Set-Name() {
     }
 }
 
-Set-Name
-Set-GradYear
+function Set-Description() {
+    Write-Host "What would you like the user's description to be?"
+    $script:DESCRIPTION = Read-Host "Description"
+}
+
+function Get-AccountType() {
+    Write-Host "Select what type of user you would like to create:
+1) Teacher
+2) Student
+3) Non-Teaching Staff
+"
+    $ACCOUNTTYPE = Read-Host "Choice"
+    Set-Name
+
+    if ($ACCOUNTTYPE -match "1") {
+        # Teacher
+        $script:EMAIL=$FNAME[0]+$LNAME
+        $script:HOME_PATH="\\ehps-fs3\teachers\$FNAME $LNAME"
+        $script:GROUPS += "Teacher" 
+        $script:GROUPS += "PSO_Teachers" 
+        $script:GROUPS += "GCDS_StandardStaff" 
+        $script:OU = "OU=Teachers"
+        Set-Description
+    }
+    elseif ($ACCOUNTTYPE -match "2") {
+        # Student
+        Set-GradYear
+        $script:EMAIL=$FNAME[0]+$LNAME+$GRADY[2]+$GRADY[3]
+        $script:HOME_PATH="\\ehps-fs3\student\$GRADY\$LNAME$FNAME"
+        $script:GROUPS += "Students"
+
+        $YEAR=$null
+        # Calculate school year
+        if ([int]$(get-date -f MM) -gt 6) { $script:YEAR = $([int]$(get-date -f yyyy))+1 }
+        else { $script:YEAR = get-date -f yyyy }
+
+        # If student is > 3rd grade
+        if ([int]$YEAR-[int]$GRADY+12 -gt 3) { $script:GROUPS += "GCDS_StandardStudent" }
+
+        # This is a dumb way to do this, but it requires "less" boiler-plate than checking every date range
+        $YTG = [int]$GRADY-[int]$YEAR
+        $SCHOOLS = @("EHHS",   "EHHS",   "EHHS",  "EHHS",
+                     "EVMS",   "EVMS",   "EVMS", 
+                     "Radley", "Radley", "Radley",
+                     "PPE",    "PPE",
+                     "Eastgate", "Eastgate")
+        $script:OU = "OU=$GRADY,OU=$($SCHOOLS[[int]$YTG]),OU=Students"
+        $script:DESCRIPTION=$GRADY
+    }
+    elseif ($ACCOUNTTYPE -match "3") {
+        # Non-teaching staff
+        $script:EMAIL=$FNAME[0]+$LNAME
+        $script:HOME_PATH="\\ehps-fs3\student\Classified Staff\"
+        $script:GROUPS += "PSO_SupportStaff" 
+        $script:GROUPS += "GCDS_StandardStaff" 
+        $script:OU = "OU=SupportStaff"
+        Set-Description
+    }
+    else {
+        Write-Host "Invalid option '$ACCOUNTTYPE'"
+        Get-AccountType
+    }
+}
 
 $CNG_PASS_RESPONSE = Read-host "Should user change password at next logon? [y/N]"
 if ($CNG_PASS_RESPONSE -match "(y|Y)") { $script:CHANGE_PASSWORD_AT_LOGON=$true }
 else { $script:CHANGE_PASSWORD_AT_LOGON=$false }
 
-if ($IS_TEACHER) {
-    # Teacher
-    $script:EMAIL=$FNAME[0]+$LNAME
-    $script:HOME_PATH="\\ehps-fs3\teachers\$FNAME $LNAME"
-    $script:GROUPS += "Teacher" 
-    $script:GROUPS += "PSO_Teachers" 
-    $script:GROUPS += "GCDS_StandardStaff" 
-    $script:OU = "OU=Teachers"
-} else {
-    # Student
-    $script:EMAIL=$FNAME[0]+$LNAME+$GRADY[2]+$GRADY[3]
-    $script:HOME_PATH="\\ehps-fs3\student\$GRADY\$LNAME$FNAME"
-    $script:GROUPS += "Students"
-
-    $YEAR=$null
-    # Calculate school year
-    if ([int]$(get-date -f MM) -gt 6) { $script:YEAR = $([int]$(get-date -f yyyy))+1 }
-    else { $script:YEAR = get-date -f yyyy }
-
-    # If student is > 3rd grade
-    if ([int]$YEAR-[int]$GRADY+12 -gt 3) { $script:GROUPS += "GCDS_StandardStudent" }
-
-    # This is a dumb way to do this, but it requires "less" boiler-plate than checking every date range
-    $YTG = [int]$GRADY-[int]$YEAR
-    $SCHOOLS = @("EHHS",   "EHHS",   "EHHS",  "EHHS",
-                 "EVMS",   "EVMS",   "EVMS", 
-                 "Radley", "Radley", "Radley",
-                 "PPE",    "PPE",
-                 "Eastgate", "Eastgate")
-    $script:OU = "OU=$GRADY,OU=$($SCHOOLS[[int]$YTG]),OU=Students"
-    $script:DESCRIPTION=$GRADY
-}
-# TODO this name cannot be longer than 20 char combined when going in the -Name field. Shorted then
-$CONCAT_NAME=$($LNAME+$FNAME) -replace '[^a-zA-Z0-9\.]', ''
+$tmpsam = $($EMAIL -replace '[^a-zA-Z0-9\.]', '')
 
 New-ADUser -HomeDrive "T:" `
     -HomeDirectory $HOME_PATH  `
     -UserPrincipalName "$EMAIL@ehps.com" `
-    -sAMAccountName $($EMAIL -replace '[^a-zA-Z0-9\.]', '') `
+    -sAMAccountName $(if ($tmpsam.Length -gt 20) { $tmpsam.Substring(0, 20) } else { $tmpsam }) `
     -GivenName $FNAME `
     -Surname $LNAME `
-    -Name $CONCAT_NAME `
+    -Name $($($LNAME+$FNAME) -replace '[^a-zA-Z0-9\.]', '') `
     -Description $DESCRIPTION `
     -EmailAddress "$EMAIL@ehps.k12.mt.us" `
     -ScriptPath "logon.bat" `
