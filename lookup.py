e739108bf1e56f28783856746b134850b86f9fad
Oliver Atkinson <oatkinson@ehps.k12.mt.us>
2024-01-30 10:55:02 -0700

    abstract script more

--- lookup.py
+++ lookup.py
@@ -6,16 +6,22 @@ import os
 
 if __name__ == "__main__":
     xkey = os.environ["XKEY"]
+    auxRow = "Hits"
+    inputIp = "IP address"
+
     with open(sys.argv[1], newline='') as csvfile:
         reader = csv.DictReader(csvfile)
+        # Collum names of CSV
+        print(f'Country Name, ISP, IP, ASN, {auxRow}')
         for row in reader:
-            user=row['User']
-            ipaddr=row['IP address']
+            aux=row[auxRow]
+            ipaddr=row[inputIp]
             response = requests.get("http://v2.api.iphub.info/ip/" + ipaddr, headers={"X-Key":xkey})
             if (response.status_code == 200):
                 # we're chillen
                 body = json.loads(response.content)
-                print(body["countryName"], user, ipaddr)
+                # CSV Line
+                print(f'{body["countryName"]},{body["isp"]},{body["ip"]},{body["asn"]},{aux}')
             if (response.status_code == 429):
                 # too many requests today :_(
                 print("Too many requests today (Error 429)")
